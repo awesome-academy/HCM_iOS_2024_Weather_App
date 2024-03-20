510fc7e4c4afa9c451170aa75a2e9a8a8d663cc6
ho.bao.an <ho.bao.an@suns-Mac-mini-2.local>
2024-03-20 15:47:59 +0700

    initial project: MVC, launch screen, tabbar

---
+++ Weather_App/Weather_App/Scenes/Map/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Weather_App
+//
+//  Created by ho.bao.an on 20/03/2024.
+//
+
+import UIKit
+
+final class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
+
