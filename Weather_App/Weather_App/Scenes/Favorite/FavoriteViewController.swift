79f3bd1a44e2439afc49594a783ab1204cd9b2fd
ho.bao.an <ho.bao.an@suns-Mac-mini-2.local>
2024-03-20 15:47:59 +0700

    initial project: MVC, launch screen, tabbar

---
+++ Weather_App/Weather_App/Scenes/Favorite/FavoriteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteViewController.swift
+//  Weather_App
+//
+//  Created by ho.bao.an on 20/03/2024.
+//
+
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Weather_App/Weather_App/Scenes/Weather Detail/WeatherDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherDetailViewController.swift
+//  Weather_App
+//
+//  Created by ho.bao.an on 20/03/2024.
+//
+
+import UIKit
+
+final class WeatherDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
