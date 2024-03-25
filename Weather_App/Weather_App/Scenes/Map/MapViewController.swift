cac997fc41b9d4859f30df444d8b783d5814c5d7
anhb-3436 <ho.bao.an@suns-Mac-mini-2.local>
2024-03-25 16:57:41 +0700

    Map Screen: Show user location and button get user location

--- Weather_App/Weather_App/Scenes/Map/MapViewController.swift
+++ Weather_App/Weather_App/Scenes/Map/MapViewController.swift
@@ -19,23 +19,58 @@ final class MapViewController: UIViewController {
     @IBOutlet weak var statusImageView: UIImageView!
     
     private let searchController = UISearchController()
-    private let cornerRadius = 15.0
+    private var location = (latitude: 0.0, longitude: 0.0)
+    
+    private var locationManager = LocationManager.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpUI()
+        configureMap()
+        locationManager.delegate = self
+    }
+    
+    override internal func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        centerMapOnUserLocation()
     }
 }
 
 extension MapViewController {
     func setUpUI() {
-        informationWeatherView.layer.cornerRadius = cornerRadius
+        informationWeatherView.layer.cornerRadius = Constants.cornerRadius
         informationWeatherView.addShadow()
-        statusView.layer.cornerRadius = cornerRadius
+        statusView.layer.cornerRadius = Constants.cornerRadius
         statusView.addShadow()
         navigationItem.searchController = searchController
         customizeNavigationController()
         customizeSearchController(searchController: searchController)
     }
+    
+    private func configureMap() {
+        mapView.showsUserLocation = true
+        centerMapOnUserLocation()
+    }
+    
+    private func centerMapOnUserLocation() {
+        if let userLocation = locationManager.getCurrentLocation() {
+            let region = MKCoordinateRegion(
+                center: userLocation.coordinate,
+                latitudinalMeters: Constants.latitudinalMeters,
+                longitudinalMeters: Constants.longitudinalMeters
+            )
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    @IBAction private func getUserLocationButtonTapped(_ sender: Any) {
+        centerMapOnUserLocation()
+    }
 }
 
+extension MapViewController: LocationManagerDelegate {
+    func didUpdateLocation(latitude: Double, longitude: Double) {
+        location.latitude = latitude
+        location.longitude = longitude
+    }
+}
---
+++ Weather_App/Weather_App/Ultilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Weather_App
+//
+//  Created by ho.bao.an on 26/03/2024.
+//
+
+import Foundation
+import CoreLocation
+
+struct Constants {
+    static let cornerRadius: CGFloat = 15.0
+    static let latitudinalMeters: CLLocationDistance = 3000
+    static let longitudinalMeters: CLLocationDistance = 3000
+}
