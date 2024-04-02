47f093d402b3d1178c340166491dd16583e07577
anhb-3436 <ho.bao.an@suns-Mac-mini-2.local>
2024-04-02 15:37:35 +0700

    Save and load data from CoreData

--- Weather_App/Weather_App/App/AppDelegate.swift
+++ Weather_App/Weather_App/App/AppDelegate.swift
@@ -9,19 +9,25 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    private let weatherCoreDataManager = WeatherCurrentCoreDataManager.shared
+    private let networkMonitor = NetworkMonitor.shared
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        networkMonitor.startMonitoring()
         return true
     }
-
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        weatherCoreDataManager.deleteWeatherNotUsed()
+    }
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
 }
 
--- Weather_App/Weather_App/Ultilities/WeatherCurrentExtension.swift
+++ Weather_App/Weather_App/Ultilities/WeatherCurrentExtension.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 extension WeatherCurrent {
+    
     var temperatureInCelsius: String {
         let temperatureCelsius = Int(self.temperatureCurrent.temperature)
         return "\(temperatureCelsius)℃"
@@ -16,4 +17,38 @@ extension WeatherCurrent {
     var weatherStatus: String? {
         return self.weathers.first?.icon
     }
+    
+    var humidityString: String {
+        return "\(self.temperatureCurrent.humidity)%"
+    }
+    
+    var cloudsString: String {
+        return "\(self.clouds.numberOfClouds)%"
+    }
+    
+    var windString: String {
+        return "\(self.wind.speed) m/s"
+    }
+    
+    var dateString: String {
+        let timestamp = TimeInterval(self.dateTime)
+        let date = Date.dateFromTimestamp(timestamp)
+        let dateString = date.convertToDateString()
+        return "\(dateString)"
+    }
+    
+    var sunriseString: String {
+        let timestamp = TimeInterval(self.timeOfSun.sunrise)
+        let date = Date.dateFromTimestamp(timestamp)
+        let timeString = date.convertToTimeString()
+        return "\(timeString)"
+    }
+    
+    var sunsetString: String {
+        let timestamp = TimeInterval(self.timeOfSun.sunset)
+        let date = Date.dateFromTimestamp(timestamp)
+        let timeString = date.convertToTimeString()
+        return "\(timeString)"
+    }
 }
+
